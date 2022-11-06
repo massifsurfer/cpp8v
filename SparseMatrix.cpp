1cee651d48108de0cc2ebf94ceb7cad3b43cdfef
Vlad Grigorian <vladisgrigr@gmail.com>
2022-11-06 14:03:18 +0300

    initial commit, project's general structure

---
+++ SparseMatrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vlad on 11/6/22.
+//
+
+#include "SparseMatrix.h"
---
+++ SparseMatrix.h
@@ -0,0 +1,14 @@
+//
+// Created by vlad on 11/6/22.
+//
+
+#ifndef CPP8V_SPARSEMATRIX_H
+#define CPP8V_SPARSEMATRIX_H
+
+
+class SparseMatrix {
+
+};
+
+
+#endif //CPP8V_SPARSEMATRIX_H
